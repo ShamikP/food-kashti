5b17bb85203115c540cdad925d8f10a357d126a0
ShamikP <smpatel2353@gmail.com>
2026-06-04 12:16:58 +0530

    Fix Vercel case sensitive imports

--- components/layout/site-layout.tsx
+++ components/layout/site-layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import WhatsAppButton from "@/components/layout/whatsapp-button";
 
 type SiteLayoutProps = {
